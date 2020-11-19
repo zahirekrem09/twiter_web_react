fbc382abd9df9f996c291481f645577dc494e899
zahirekrem09 <1820980144@stu.adu.edu.tr>
2020-11-19 19:21:18 +0200

    add svg icons

---
+++ stories/Icons.stories.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Button from "../components/Button";
+
+import * as Icons from "../components/icons";
+
+export default {
+  title: "Icons",
+};
+
+export const Icon = () => {
+  return (
+    <div>
+      <Icons.BookmarkFill />
+    </div>
+  );
+};
